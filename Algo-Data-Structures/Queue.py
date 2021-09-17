37189f076c53c8ed1bc632e62f02c437abe2ea0c
Eric palumbo <ep85@njit.edu>
2021-09-17 16:45:34 -0400

    Update Queue.py

--- Algo-Data-Structures/Queue.py
+++ Algo-Data-Structures/Queue.py
@@ -24,3 +24,19 @@ queue.pop()
 # test3
 
 
+#examples
+#Write a program to print binary numbers from 1 to 10 using Queue. Use Queue class implemented in main tutorial. Binary sequence should look like,
+def produce_binary_numbers(n):
+    numbers_queue = deque()
+    numbers_queue.appendleft("1")
+
+    for i in range(n):
+        front = numbers_queue[-1]
+        print("   ", front)
+        numbers_queue.appendleft(front + "0")
+        numbers_queue.appendleft(front + "1")
+
+        numbers_queue.pop()
+
+
+produce_binary_numbers(10)
